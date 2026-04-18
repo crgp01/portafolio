2e0b575358d368e5305671600df22ee60bc0280e
Cristina Rivera <riverap.cristina@gmail.com>
2026-04-18 14:29:49 +0200

    feat: Footer con redes sociales

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import Hero from './components/Hero'
 import About from './components/About'
 import Portfolio from './components/Portfolio'
 import Contact from './components/Contact'
+import Footer from './components/Footer'
 
 function App() {
   return (
@@ -14,6 +15,7 @@ function App() {
         <Portfolio />
         <Contact />
       </main>
+      <Footer />
     </div>
   )
 }
